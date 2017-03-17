52a0d4247e969b3d38eb224334662488979a3896
Danny Gilbert <gilbert.daniel@heb.com>
2017-03-17 14:05:50 -0500

    Completed my first iOS framework

---
+++ RGB/RGBUIColor.swift
@@ -0,0 +1,23 @@
+//
+//  RGBUIColor.swift
+//  RGB
+//
+//  Created by Danny Gilbert on 3/17/17.
+//  Copyright © 2017 Danny Gilbert. All rights reserved.
+//
+
+import Foundation
+import Curry
+
+public func RGBUIColor(red: Int, green: Int, blue: Int) -> UIColor {
+    return curry(createColor)(red)(green)(blue)
+}
+
+private func createColor(red: Int, green: Int, blue: Int) -> UIColor {
+    return UIColor(
+        red: CGFloat(red/255),
+        green: CGFloat(green/255),
+        blue: CGFloat(blue/255),
+        alpha: 1
+    )
+}
