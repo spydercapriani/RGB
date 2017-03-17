75fc196e9377b7f29347587630e2a10242f05fd8
Danny Gilbert <gilbert.daniel@heb.com>
2017-03-17 12:14:07 -0500

    Initial Commit

---
+++ RGB/RGB.h
@@ -0,0 +1,19 @@
+//
+//  RGB.h
+//  RGB
+//
+//  Created by Danny Gilbert on 3/17/17.
+//  Copyright © 2017 Danny Gilbert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RGB.
+FOUNDATION_EXPORT double RGBVersionNumber;
+
+//! Project version string for RGB.
+FOUNDATION_EXPORT const unsigned char RGBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGB/PublicHeader.h>
+
+
